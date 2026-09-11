1d55ab4d54f23d4d269357eaa5f36720d15f36b6
Joel Baez <joelbaezcz@gmail.com>
2026-09-11 11:07:21 -0600

    provisional

--- src/global/sepomex/sepomex.config.ts
+++ src/global/sepomex/sepomex.config.ts
@@ -5,8 +5,8 @@ export const SEPOMEX_DEV_BASE_URL = "https://sepomex.kurenn.dev";
 
 export const SEPOMEX_BASE_URL =
     nodeEnv === "production" || nodeEnv === "testing"
-        ? SEPOMEX_PROD_BASE_URL
-        : SEPOMEX_DEV_BASE_URL;
+        ? SEPOMEX_DEV_BASE_URL
+        : SEPOMEX_PROD_BASE_URL;
 
 export const SEPOMEX_ZIP_CODE_ENDPOINT = "/api/v1/zip_codes";
 
